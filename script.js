85623766faad5306ddf65f981864da8109e39f25
sagargkunte <sagargkunte24@gmail.com>
2025-06-02 12:51:26 +0530

    modified the html and js file and added separate css file
    modified the html and js file and added separate css file. Before i wrote code only for color name now i wrote for all including hex code and rgb code

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const input = document.querySelector('.input');
-const body = document.querySelector('body');
+const input = document.querySelector(".input");
+const body = document.querySelector("body");
 
 function Execute() {
-    let value = input.value;
+  let value = input.value;
+  if (isValidColor(value)) {
     body.style.background = value;
+  } else {
+    alert(" ❌ Enterd text is not valid color name ❗");
+  }
+  // body.style.background = value;
 }
+
+function isValidColor(strColor) {
+  const s = new Option().style;
+  console.log(s);
+  s.color = strColor;
+
+  return s.color !== "";
+}
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log(e.target.id);
+    body.style.background = e.target.id;
+  });
+});
